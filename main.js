7e16b4a629f0e8c9056b2922d6e59cd1f6d3f299
Ana Landin <alandin1@ucol.mx>
2019-09-17 19:39:47 -0500

    clase

---
+++ main.js
@@ -0,0 +1,38 @@
+/* 
+Universidad de Colima 
+Facultad de Telemática
+Fundamentos de programación
+*/
+
+var nombre = 'Juan Pérez'
+var edad = 22
+var esEstudiante = true
+var email 
+var numero1 = 5
+var numero2 = 2
+
+console.log(50 + 10 / 8 + 2)
+console.log((50 + 10) / (8 + 2))
+
+console.log(numero1 + numero2)
+console.log(numero1 - numero2)
+console.log(numero1 * numero2)
+console.log(numero1 / numero2)
+console.log(numero1 % numero2)
+
+var resultado = numero1 + numero2
+console.log(resultado)
+
+// Ejemplo de Conceptos básicos
+console.log(nombre)
+console.log(edad)
+console.log(email)
+console.log ('Hola, Mundo!')
+
+console.log(
+    nombre + 'tiene' + edad + ' años. ¿Es estudiante?' + esEstudiante 
+)
+
+nombre= prompt('¿Cómo te llamas?')
+
+alert('Hola ' + nombre)
